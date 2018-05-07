0ec688fe7d0ee5952aecca0b7ed7a1f1d03203cd
regre <abdullah.fachmi@gmail.com>
2018-05-07 08:37:03 +0700

    Initial commit

---
+++ resources/assets/js/Youtube/VideoGroup.vue
@@ -0,0 +1,58 @@
+<template>
+	<div class="VideoGroup__wrapper row grid">
+		<video-item 
+			v-images-loaded:on.progress="imageProgress"
+			v-for="video in videos" 
+			:video="video"
+			:key="video.id.videoId">
+		</video-item>
+	</div>
+</template>
+
+
+<script>
+	import Isotope from 'isotope-layout';
+	import imagesLoaded from 'vue-images-loaded';
+	import VideoItem from './VideoItem.vue';
+	export default {
+		props: ['videos'],
+
+		components: {
+			VideoItem
+		},
+
+		created() {
+			this.relayoutTheGrid();
+		},
+
+		directives: {
+			imagesLoaded
+		},
+
+		data() {
+			return {
+				isotope: null,
+				counter: 0
+			}
+		},
+
+		methods: {
+			relayoutTheGrid() {
+			setTimeout(() => {
+				var elem = document.querySelector('.grid');
+				this.isotope = new Isotope( elem, {
+					itemSelector: '.card',
+					layoutModel: 'masonary'
+				});	
+			}, 1);
+				
+			},
+			imageProgress(instance, img) {
+				this.counter++;
+				if(this.counter == this.videos.length) {
+					this.relayoutTheGrid();
+				}
+			}
+		}
+	}
+</script>
---
+++ resources/assets/js/Youtube/YoutubeDash.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="YoutubeDash__wrapper">
+		<video-group :videos="videos"></video-group>
+	</div>
+</template>
+
+
+<script>
+import VideoGroup from './VideoGroup.vue';
+import Search from './Search';
+
+export default {
+	components: {
+		VideoGroup
+	},
+	created() {
+		Search({
+			apiKey: 'AIzaSyBFccUWWWwugZQ38p8Se4B8NELhkL4zxyc',
+			term: 'avengers infinity war',
+			items: 10
+		}, response => this.videos = response);			
+	},
+
+	data() {
+		return {
+		videos: null
+		}
+	}
+}
+</script>
